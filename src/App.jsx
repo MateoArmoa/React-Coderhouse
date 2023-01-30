2912a1023c2c14ef6dfcdf799ab367603b4ec298
Mateo Armoa <tutecarc98@hotmail.com>
2023-01-30 18:02:35 -0300

    agrego bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from './componentes/NavBar/NavBar';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import ItemListContainer from './componentes/ItemListContainer/ItemListContainer';
+
+function App(props) { 
+   
+    return (
+        <>
+            <NavBar />
+                 </>
+       
+    )
+}
+
+export default App
+
---
+++ src/componentes/NavBar/Navbar.jsx
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from '../CartWidget/CartWidget';
+
+const NavBar = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">RunyIND</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#">Remeras</Nav.Link>
+                    <Nav.Link href="#">Bermudas</Nav.Link>
+                    
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
