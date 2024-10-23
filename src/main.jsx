cf35ee3edc339e3093c509dd99f4061388200376
MateoArmoa <103545335+MateoArmoa@users.noreply.github.com>
2024-10-23 00:44:37 -0300

    Arreglos en el reactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 App() > <App />
 
-ReactDOM.createRoot(document.getElementById('root')).render( <App /> )
+ReactDOM.render(<App />, document.getElementById('root'));
   
